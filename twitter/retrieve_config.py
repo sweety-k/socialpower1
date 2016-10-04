dd6f780f57be3dd7b01fb69c2790693d8392cd80
Ushasree Ginne <ushasree.ginne@gramener.com>
2016-10-04 12:29:54 +0000

    key authentication

---
+++ twitter/retrieve_config.py
@@ -0,0 +1,5 @@
+"""Configuration File for Tweet Recovery."""
+CONSUMER_KEY = '8wTeesdKMnjWzWnCpngAyx7zZ'
+CONSUMER_SECRET = 'Yj6FVS8GiXWj17apkSsRNAgielCuoKs5rSrP9dycnVg2Fdp9Pa'
+ACCESS_TOKEN = '603487927-u1NiV4sk971JuMuBu3EzCT39nqnlnztrk6w5nTXi'
+ACCESS_TOKEN_SECRET = 'AVDaHk0p88LjP0xahEqBiqIqofgKqeKzaBx8vrqAQQB2D'
